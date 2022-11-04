c84214f1a30117214c6a83e2bcbe79344d81342b
LZY_Shrio <170280176@qq.com>
2022-11-04 18:07:38 +0800

    fix：damn it

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -6,7 +6,7 @@ interface IProps {
   isLoaded: boolean;
 }
 
-export default function HomeLoading({ isLoaded }: IProps) {
+const HomeLoading: React.FC<IProps> = ({ isLoaded }) => {
   const [isShow, setIsShow] = useState<boolean>(true);
   useEffect(() => {
     if (!isLoaded) {
@@ -16,30 +16,33 @@ export default function HomeLoading({ isLoaded }: IProps) {
     }
   }, [isLoaded]);
   return (
-    isShow && (
-      <div
-        className={classnames(
-          styles.loading_content,
-          isLoaded && styles.loading_content_fade
-        )}
-      >
-        <div className={styles.spinner_box}>
-          <div className={styles.configure_border_1}>
-            <div className={styles.configure_core}></div>
+    <>
+      {isShow && (
+        <div
+          className={classnames(
+            styles.loading_content,
+            isLoaded && styles.loading_content_fade
+          )}
+        >
+          <div className={styles.spinner_box}>
+            <div className={styles.configure_border_1}>
+              <div className={styles.configure_core}></div>
+            </div>
+            <div className={styles.configure_border_2}>
+              <div className={styles.configure_core}></div>
+            </div>
           </div>
-          <div className={styles.configure_border_2}>
-            <div className={styles.configure_core}></div>
+          <div className={styles.cube_wrapper}>
+            <div className={styles.cube_folding}>
+              <span className={styles.leaf1}></span>
+              <span className={styles.leaf2}></span>
+              <span className={styles.leaf3}></span>
+              <span className={styles.leaf4}></span>
+            </div>
           </div>
         </div>
-        <div className={styles.cube_wrapper}>
-          <div className={styles.cube_folding}>
-            <span className={styles.leaf1}></span>
-            <span className={styles.leaf2}></span>
-            <span className={styles.leaf3}></span>
-            <span className={styles.leaf4}></span>
-          </div>
-        </div>
-      </div>
-    )
+      )}
+    </>
   );
-}
+};
+export default HomeLoading;
