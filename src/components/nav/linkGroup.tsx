af044dcf0be155b7c52aa89434f70f9ea5f0eb73
LZY_Shrio <170280176@qq.com>
2023-02-05 11:55:44 +0800

    style: nav link underline

--- src/components/nav/linkGroup.tsx
+++ src/components/nav/linkGroup.tsx
@@ -2,13 +2,21 @@ import Link from 'next/link';
 import styles from './index.module.scss';
 export default function linkGroup() {
   return (
-    <nav className="flex items-center mr-6">
+    <nav className="flex items-center mr-10">
       <ul className="flex items-center">
-        <li className="mx-2 cursor-pointer">
-          <Link href={'/'}>Home</Link>
+        <li className="mx-4 cursor-pointer">
+          <Link href={'/'} className={styles.nav__link} data-name="Home">
+            Home
+          </Link>
         </li>
-        <li className="mx-2 cursor-pointer">
-          <Link href={'/about'}>About</Link>
+        <li className="mx-4 cursor-pointer">
+          <Link
+            href={'/about'}
+            className={styles.nav__link}
+            data-name="About"
+          >
+            About
+          </Link>
         </li>
       </ul>
     </nav>
