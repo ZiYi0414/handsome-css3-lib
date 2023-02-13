bd6cea270ea53310d15750f96fbc2af41e0bc165
LZY_Shrio <170280176@qq.com>
2023-02-13 18:16:38 +0800

    feat: bala bala

--- src/components/nav/phoneGroup.tsx
+++ src/components/nav/phoneGroup.tsx
@@ -8,26 +8,24 @@ export default function PhoneGroup() {
   const { theme, setTheme } = useTheme();
   const [showSideMenu, setShowSideMenu] = useState(false);
   return (
-    <div>
-      <div className={styles.phoneGroup__wrap}>
-        <button
-          className="mr-4"
-          onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-        >
-          <span
-            className={classNames(
-              'iconfont !text-[25px]',
-              theme === 'dark' ? 'icon-moon' : 'icon-sun'
-            )}
-          />
-        </button>
-        <div className={styles.bar__btn} onClick={() => setShowSideMenu(true)}>
-          <label className={styles.bar}>
-            <span className={styles.top}></span>
-            <span className={styles.middle}></span>
-            <span className={styles.bottom}></span>
-          </label>
-        </div>
+    <div className={styles.phoneGroup__wrap}>
+      <button
+        className="mr-4"
+        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      >
+        <span
+          className={classNames(
+            'iconfont !text-[25px]',
+            theme === 'dark' ? 'icon-moon' : 'icon-sun'
+          )}
+        />
+      </button>
+      <div className={styles.bar__btn} onClick={() => setShowSideMenu(true)}>
+        <label className={styles.bar}>
+          <span className={styles.top}></span>
+          <span className={styles.middle}></span>
+          <span className={styles.bottom}></span>
+        </label>
       </div>
       <div
         className={classNames(
