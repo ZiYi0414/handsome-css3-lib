0b87f0468846d293f969628df43ef08910a1188b
LZY_Shrio <170280176@qq.com>
2023-02-15 18:02:32 +0800

    feat: add analytics in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,14 @@ import '@/styles/globals.scss';
 import '@/styles/dracula.scss';
 import '../../public/iconfont/iconfont.css';
 import { ThemeProvider } from 'next-themes';
-
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
+      <Analytics mode={'production'} debug={false} />
     </ThemeProvider>
   );
 }
