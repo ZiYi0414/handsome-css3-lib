d5ce075dcb18646fb66bb7df595c88a05f60b2d9
Luyiin <170280176@qq.com>
2023-02-28 18:01:20 +0800

    feat: banner and footer update

--- src/components/content/banner/index.tsx
+++ src/components/content/banner/index.tsx
@@ -2,7 +2,7 @@ import { useTheme } from 'next-themes';
 import styles from '../index.module.scss';
 export default function Banner() {
   const { resolvedTheme } = useTheme();
-  
+
   return (
     <div className={styles.star__background}>
       <div className={styles.container}>
@@ -37,41 +37,14 @@ export default function Banner() {
             <span></span>
           </div>
         )}
-        <div className={styles.media__title}>{`<AWA UI />`}</div>
-        <h1 id={styles.master}>
-          <div className={styles.title}>{`<AWA UI />`}</div>
-          <div id={styles.scroller}>
-            <div className={styles.scroller__item}>
-              “ 并且完全开源 MIT License. ”{' '}
-            </div>
-            <div className={styles.scroller__item}>
-              “ 来自于互联网各国平面设计师. ”{' '}
-            </div>
-            <div className={styles.scroller__item}>
-              “ 使用纯HTML + CSS 动画实现. ”
-            </div>
-            <div className={styles.scroller__item}>
-              “ 无任何JavaScript的CSS3动画. ”
-            </div>
-            <div className={styles.scroller__item}>
-              “ FxxK JS, 纯爷们只用CSS. ”
-            </div>
-            <div className={styles.scroller__item}>
-              “ 来自于海内外平面设计师. ”
-            </div>
-            <div className={styles.scroller__item}>“ Copy到你的项目中. ”</div>
-            <div className={styles.scroller__item}>“ 帮助你提高审美. ”</div>
-            <div className={styles.scroller__item}>
-              “ Funny coder （肥宅或艺术家 ”
-            </div>
-            <div className={styles.scroller__item}>
-              “ 并且完全开源 MIT License. ”
-            </div>
-          </div>
-        </h1>
+        <div className={styles.title}>
+          Fall in <span className="text-[#ff5858]">love</span> with
+          <span className="text-[#0071e2]"> css</span>
+        </div>
         <div className={styles.sub__title}>
           <h2>
-            Amazingly handsome Open-Source UI components made with HTML and CSS
+            Amazingly <span>handsome Open-Source</span> UI components made with
+            HTML and CSS
           </h2>
           <p>
             Thank you to everyone who contributes to the cause and art of open
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,18 +2,9 @@ import { useState } from 'react';
 import classNames from 'classnames';
 import NextImage from 'image/NextImage';
 import styles from './index.module.scss';
-import Avator1 from '@/assets/img/ziyi0414.png';
-import Avator3 from '@/assets/img/ziyi0414.gif';
-
+import Avator from '@/assets/img/ziyi0414.png';
 
 export default function Footer() {
-  const [avator, setAvator] = useState(Avator3);
-
-  const handleChangeImg = () => {
-    const imgArr = [Avator1, Avator3];
-    setAvator(imgArr[Math.floor(Math.random() * imgArr.length)]);
-  };
-
   return (
     <footer
       className={classNames(styles.footer, 'my-16 text-center text-[#b3b3b3] ')}
@@ -21,8 +12,7 @@ export default function Footer() {
       <section className="flex flex-col items-center">
         <div className={styles.avator}>
           <NextImage
-            onClick={handleChangeImg}
-            src={avator}
+            src={Avator}
             alt="我"
             width={99}
             height={99}
