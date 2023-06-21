0f9379fbdc423f89c969b49af78cde72e165ceb7
Luyiin <170280176@qq.com>
2023-06-21 17:23:53 +0800

    fix: fix header logo theme color

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,18 +1,13 @@
-import { useMemo } from 'react';
 import dynamic from 'next/dynamic';
 import styles from './index.module.scss';
 import LinkGroup from '@/components/nav/linkGroup';
 import PhoneGroup from '@/components/nav/phoneGroup';
 import classNames from 'classnames';
-import NextImage from 'image/NextImage';
 import { useTheme } from 'next-themes';
-import Logo from '@/assets/img/logo.png';
-import Logo_dark from '@/assets/img/logo_dark.png';
 import Link from 'next/link';
 
 const IconGroup = dynamic(import('@/components/nav/iconGroup'), { ssr: false });
 export default function Header() {
-  const { resolvedTheme } = useTheme();
 
   return (
     <header className="flex relative">
@@ -20,13 +15,7 @@ export default function Header() {
         <div className={styles.header}>
           <Link href={'/'}>
             <div className={styles.header__title}>
-              <NextImage
-                src={resolvedTheme === 'dark' ? Logo : Logo_dark}
-                alt="AWA UI LOGO"
-                width={38}
-                height={38}
-                draggable={false}
-              ></NextImage>
+              <span className="iconfont icon-awa-logo ml-2 !text-[32px]" />
               <p className="ml-2">{`AwA UI`}</p>
             </div>
           </Link>
